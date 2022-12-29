fe224ff0ffac4a24ff540d8906841a08987456f2
rajala99 <rajala.harini99@gmail.com>
2022-12-29 18:43:14 +0530

    ADDED: code for welcome message

---
+++ src/com/bridgelabs/addressbook/Address_Book_Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabs.addressbook;
+
+public class Address_Book_Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address book program");
+    }
+}
